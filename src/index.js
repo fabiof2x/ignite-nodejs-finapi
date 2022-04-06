31f85d0ec05b26caeada3269b4d97e58925a2b18
fabiof2x <fabioar@gmail.com>
2022-04-06 11:14:49 -0300

    Criando depósito na conta

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
-
 const app = express();
-
 app.use(express.json());
 
 const customers = [];
@@ -10,7 +8,6 @@ const customers = [];
 // Middleware
 function verifyIfExistsAccountCPF(request, response, next) {
   const { cpf } = request.headers;
-
   const customer = customers.find(customer => customer.cpf === cpf);
 
   if (!customer) {
@@ -18,13 +15,11 @@ function verifyIfExistsAccountCPF(request, response, next) {
   }
 
   request.customer = customer;
-
   return next();
 }
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
-
   const customerAlreadyExists = customers.some(
     (customer) => customer.cpf === cpf
   );
@@ -49,6 +44,21 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
 
   return response.json(customer.statement)
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
